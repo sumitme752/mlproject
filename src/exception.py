e9a060cf2a90cd0572b2d2e6e7afe864ccc66aaf
sumitme752 <sumitme752@gmail.com>
2023-03-14 00:19:10 +0530

    EDA and Model training

--- src/exception.py
+++ src/exception.py
@@ -17,13 +17,13 @@ class CustomException(Exception):
         def __str__(self):
             return self.error_message    
         
-
-if __name__=="__main__":
+#testing code
+# if __name__=="__main__":
      
-    try:
-          a=1/0
+#     try:
+#           a=1/0
 
-    except Exception as e:
-         logging.info("Divided by zero")
-         raise CustomException(e,sys)
+#     except Exception as e:
+#          logging.info("Divided by zero")
+#          raise CustomException(e,sys)
     
